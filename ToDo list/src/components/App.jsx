9d12155883169ca0cd41ed678e3ae01329c48a3f
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-03 19:37:44 +0530

    added the new look

--- ToDo list/src/components/App.jsx
+++ ToDo list/src/components/App.jsx
@@ -3,18 +3,33 @@ import React, { useState } from "react";
 function App() {
   const [list, setList] = useState("");
 
+  const tasks = (e)=>{
+    const newTask = e.target.value;
+    setList(prevValue =>{
+      return {
+        ...prevValue + newTask
+      }
+    })
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input onChange={tasks} type="text" />
         <button>
-          <span>Add</span>
+          <span onClick={addTask}>Add</span>
         </button>
       </div>
       <div>
         <ul>
           <li>A Item </li>
-        </u
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
